77087d62bea981f899ef40afe2249e94afc6cd53
rousbelVillar <rousbel@github.com>
2026-01-06 22:10:02 -0800

    installing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"], theme: {
+  content: ["./src/**/*.{html,js,ts,tsx}"], theme: {
     extend: {},
   },
   plugins: [],
