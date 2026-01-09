5b93f0524baff76eca4c619e5b8c102531b040f1
rousbelVillar <rousbel@github.com>
2026-01-09 15:19:24 -0800

    adding companies

--- src/Components/Companies.tsx
+++ src/Components/Companies.tsx
@@ -5,14 +5,11 @@ export const Companies = () => {
       title: "New Fire Global Partners",
       href: "#",
       description:
-        "Newfire Global Partners is an American company and a leader in software development & IT services with worldwide operations that span four continents.",
-      date: "Founded 2016",
+        "Served as a software developer of a health insurance company platform that was composed of several Angular micro-frontend applications, Golang, and MongoDB on the back-end, serving more than 500,000 users in the US.",
+      date: "Sep 2022 - Jan 2025",
       datetime: "",
-      category: { title: "Outsourcing", href: "#" },
-      author: {
-        name: "",
-        role: "",
-        href: "#",
+      category: { title: "Remote", href: "#" },
+      company: {
         imageUrl:
           "https://cookie-cdn.cookiepro.com/logos/790eb134-7059-40c3-914a-dc668ec03813/018dc6e2-dc19-7025-871d-07a76ab2693d/f9a5901d-f60d-40ab-aec9-d7e1f5e8de46/Newfire_logo.png",
       },
@@ -22,31 +19,39 @@ export const Companies = () => {
       title: "Epam Systems",
       href: "#",
       description:
-        "EPAM Systems, Inc. is an American company that specializes in software engineering services, digital platform engineering, and digital product design.",
-      date: "Founded 1993",
+        "Served as a full-stack engineer, creating Web applications for a learning web platform used across the United States, helping teachers grade, provide assignments, and learning programs to their students.",
+      date: "Sep 2021 - Sep 2022",
       datetime: "",
-      category: { title: "Outsourcing", href: "#" },
-      author: {
-        name: "",
-        role: "",
-        href: "#",
+      category: { title: "Remote", href: "#" },
+      company: {
         imageUrl:
           "https://www.eisgroup.com/wp-content/uploads/2025/07/EPAM-1.png",
       },
     },
     {
       id: 3,
-      title: "Newtech Global",
+      title: "Newtech Srl",
       href: "#",
       description:
-        "Newtech Global in the Dominican Republic is a leading Business Process Outsourcing (BPO) and technology firm, offering customer service, IT support, software development.",
-      date: "Founded 2005",
+        "Served as a front-end Engineer, creating UI interfaces and features for the second-largest telecommunications company in the United States.",
+      date: "Feb 2020 - Feb 2021",
       datetime: "",
-      category: { title: "Outsourcing", href: "#" },
-      author: {
-        name: "",
-        role: "",
-        href: "#",
+      category: { title: "Remote", href: "#" },
+      company: {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCIZBlTA4h5VTQSsDzAxAHhYLVee74rEyT1w&s",
+      },
+    },
+    {
+      id: 3,
+      title: "Newtech Srl",
+      href: "#",
+      description:
+        "Served as a front-end Engineer, creating UI interfaces and features for the second-largest telecommunications company in the United States.",
+      date: "Aug 2017 - Feb 2020",
+      datetime: "",
+      category: { title: "On-site", href: "#" },
+      company: {
         imageUrl:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCIZBlTA4h5VTQSsDzAxAHhYLVee74rEyT1w&s",
       },
@@ -55,28 +60,18 @@ export const Companies = () => {
 
   return (
     <div
-      className="mx-auto mt-10 grid max-w-2xl grid-cols-1 
-        gap-x-8 gap-y-16 border-t border-gray-700 pt-10 
-        sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-1 
-        justify-items-end"
+      className=" grid grid-cols-1 gap-y-16  pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none 
+    lg:grid-cols-1 mt-[10em] lg:mr-[10em] lg:ml-[10em] sm:mr-0 sm:ml-0 mb-20"
     >
-      <div className="absolute top-[23em]">
-        <h1 className="text-lg/8 text-gray-300 font-bold absolute left-[-63vw]">
-          {" "}
-          Experience
-        </h1>
+      <div>
+        <h1 className="text-lg/8 text-gray-300 font-bold"> Experience</h1>
       </div>
       {posts.map((post) => (
         <article
           key={post.id}
-          className="flex w-full max-w-xl flex-col items-start justify-between mt-6"
+          className="flex w-full max-w-xl flex-col items-start"
         >
           <div className="flex items-center gap-x-4 text-xs">
-            <img
-              alt=""
-              src={post.author.imageUrl}
-              className="size-10 rounded-full bg-gray-800 float-end"
-            />
             <time dateTime={post.datetime} className="text-gray-400">
               {post.date}
             </time>
@@ -86,6 +81,11 @@ export const Companies = () => {
             >
               {post.category.title}
             </a>
+            <img
+              alt=""
+              src={post.company?.imageUrl}
+              className="size-10 rounded-full bg-gray-8000"
+            />
           </div>
           <div className="group relative grow">
             <h3 className="mt-3 text-lg/6 font-semibold text-white group-hover:text-gray-300">
@@ -97,17 +97,6 @@ export const Companies = () => {
             <p className="mt-5 line-clamp-3 text-sm/6 text-gray-400">
               {post.description}
             </p>
-          </div>
-          <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-            <div className="text-sm/6">
-              <p className="font-semibold text-white">
-                <a href={post.author.href}>
-                  <span className="absolute inset-0" />
-                  {post.author.name}
-                </a>
-              </p>
-              <p className="text-gray-400">{post.author.role}</p>
-            </div>
           </div>
         </article>
       ))}
--- src/Components/ScrollSection.tsx
+++ src/Components/ScrollSection.tsx
@@ -1,9 +1,11 @@
 import { About } from "./About";
+import { Companies } from "./Companies";
 
 export const ScrollSection = () => {
   return (
-    <section className="float-end w-[50%] h-[100%]   text-white ">
+    <section className="float-end w-[50%] h-[100%] text-white">
       <About />
+      <Companies />
     </section>
   );
 };
